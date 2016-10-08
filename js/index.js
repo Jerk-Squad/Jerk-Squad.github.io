90f69f9770cc57242a40c954181937292bf5b1db
ConnorRath <ccr028@shsu.edu>
2016-10-08 11:42:03 -0500

    Testing/ practicing Onlick sound function, uploaded sound clips into sounds folder for later use.
    I feel like this websitre won't be complete without meme sound clips, so
    I was implementing the onclick with specific sound clips and just
    testing how they work. Left materialize because YOLO amirite.
    CSS has had no work done to it. Brandon hasn't message back about ideas
    for page.

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function Sad() {
+                var sad = new Audio();
+                sad.src = "sounds/2SAD4ME.mp3";
+                document.getElementById(sad);
+                sad.play();
+                    }
+
+function Horn() {
+                var horn = new Audio();
+                horn.src = "sounds/AIRHORN.mp3";
+                document.getElementById(horn);
+                horn.play();
+                    }
+
+function Dankstorm() {
+                var dankStorm = new Audio();
+                dankStorm.src = "sounds/Dankstorm.mp3";
+                document.getElementById(dankStorm);
+                dankStorm.play();
+                    }
+function Hitmarker() {
+    var hit = new Audio();
+    hit.src = "sounds/HITMARKER.mp3";
+    document.getElementById(hit);
+    hit.play();
+}
+
+
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function writeToP(stringToPrint) {
+    "use strict";
+    document.getElementById("intro").innerHTML = (stringToPrint);
+}
+
+writeToP("Welcome to Jerk Squad AI Assistant");
