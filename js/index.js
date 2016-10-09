b6d5683a813a25087e83f2181ffc73f4b00bf57f
ConnorRath <ccr028@shsu.edu>
2016-10-09 10:45:59 -0500

    Setup Header, Footer, create content for splash page.

--- js/index.js
+++ js/index.js
@@ -19,10 +19,44 @@ function Dankstorm() {
                 dankStorm.play();
                     }
 function Hitmarker() {
-    var hit = new Audio();
-    hit.src = "sounds/HITMARKER.mp3";
-    document.getElementById(hit);
-    hit.play();
-}
-
-
+                var hit = new Audio();
+                hit.src = "sounds/HITMARKER.mp3";
+                document.getElementById(hit);
+                hit.play();
+                    }
+function Mom() {
+                var mom = new Audio();
+                mom.src = "sounds/MOM GET THE CAMERA.mp3";
+                document.getElementById(mom);
+                mom.play();
+                    }
+function Triple() {
+                var trip = new Audio();
+                trip.src = "sounds/Oh Baby A Triple.mp3";
+                document.getElementById(trip);
+                trip.play();
+                    }
+function OMG() {
+                var omg = new Audio();
+                omg.src = "sounds/OMG TRICKSHOT CHILD.mp3";
+                document.getElementById(omg);
+                omg.play();
+                    }
+function OOH() {
+                var ooh = new Audio();
+                ooh.src = "sounds/OOOOOOOOHMYGOOOOD.mp3";
+                document.getElementById(ooh);
+                ooh.play();
+                    }
+function Skrill() {
+                var skrill = new Audio();
+                skrill.src = "sounds/SKRILLEX Scary.mp3";
+                document.getElementById(skrill);
+                skrill.play();
+                    }
+function Smoke() {
+                var smoke = new Audio();
+                smoke.src = "sounds/SMOKE WEEK EVERYDAY.mp3";
+                document.getElementById(smoke);
+                smoke.play();
+                    }
